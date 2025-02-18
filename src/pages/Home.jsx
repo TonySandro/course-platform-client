582775917ce9f5a83ea508d07e7459c7499b9014
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-02-18 10:38:11 -0300

    feat: initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Home = () => {
+  const navigate = useNavigate()
+  const fetchUser = async () => {
+    try {
+      const token = localStorage.getItem('token')
+      // const response = await axios.get('http://localhost:3000/auth/home', {
+      //   headers: {
+      //     "Authorization" : `Bearer ${token}`
+      //   }
+      // })
+      if(!token) {
+        navigate('/login')
+      }
+    } catch(err){
+      navigate('/login')
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    fetchUser()
+  }, [])
+  return (
+    <div className='text-3xl text-blue-500'>Home</div>
+  )
+}
+
+export default Home
