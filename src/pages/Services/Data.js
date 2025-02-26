aecf7bd9a95f8ab31531f2aeea00894ba27c7060
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-02-26 11:00:03 -0300

    feat: translate all texts of services

--- src/pages/Services/Data.js
+++ src/pages/Services/Data.js
@@ -1,14 +1,17 @@
+import img1 from '../../images/svg-1.svg';
+import img2 from '../../images/svg-2.svg';
+import img3 from '../../images/svg-3.svg';
+
 export const homeObjOne = {
     lightBg: false,
     lightText: true,
     lightTextDesc: true,
-    topLine: 'Exclusive Access',
-    headline: 'Unlimited Transactions with zero fees',
-    description:
-      'Get access to our exclusive diamond card that allows you to  send unlimited transactions without getting charged any fees',
-    buttonLabel: 'Get Started',
+    topLine: "Aprendizado Personalizado",
+    headline: "Foco nas suas necessidades e objetivos",
+    description: "Na Mentoria Individual, você recebe atenção exclusiva de uma terapeuta ABA com mais de 12 anos de experiência. Cada sessão é adaptada para atender às suas demandas específicas, garantindo um plano de ação eficaz e resultados mensuráveis.",
+    buttonLabel: "Saiba Mais",
     imgStart: '',
-    img: require('../../images/svg-1.svg'),
+    img: img1,
     alt: 'Credit Card'
   };
   
@@ -22,7 +25,7 @@ export const homeObjOne = {
       'We have you covered no matter where you are located. Over 140 locations worldwide to ensure you have access anytime',
     buttonLabel: 'Learn More',
     imgStart: '',
-    img: require('../../images/svg-1.svg'),
+    img: img1,
     alt: 'Vault'
   };
   
@@ -30,13 +33,12 @@ export const homeObjOne = {
     lightBg: true,
     lightText: false,
     lightTextDesc: false,
-    topLine: 'Easy Setup',
-    headline: 'Super fast and simple onboarding process',
-    description:
-      "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
-    buttonLabel: 'Start Now',
+    topLine: "Troca de Experiências",
+    headline: "Aprendizado colaborativo e networking",
+    description: "A Mentoria em Grupo oferece um ambiente de troca de conhecimentos e experiências com outros profissionais da área. Além de aprender com a orientação de especialistas, você amplia sua rede de contatos e compartilha desafios e soluções.",
+    buttonLabel: "Saiba Mais",
     imgStart: 'start',
-    img: require('../../images/svg-1.svg'),
+    img: img1,
     alt: 'Vault'
   };
   
@@ -50,6 +52,6 @@ export const homeObjOne = {
       'Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.',
     buttonLabel: 'Sign Up Now',
     imgStart: 'start',
-    img: require('../../images/svg-1.svg'),
+    img: img1,
     alt: 'Vault'
   };
