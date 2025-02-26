8daca6cd0aa3630c225a51bd0c327c1d5a08541f
Tony Sandro <85088970+TonySandro@users.noreply.github.com>
2025-02-26 10:35:06 -0300

    feat: comment footer sub heading

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -34,7 +34,7 @@ function Footer() {
 
   return (
     <FooterContainer>
-      <FooterSubscription>
+      {/* <FooterSubscription>
         <FooterSubHeading>
           Join our exclusive membership to receive the latest news and trends
         </FooterSubHeading>
@@ -43,7 +43,7 @@ function Footer() {
           <FormInput name='email' type='email' placeholder='Your Email' />
           <Button fontBig>Subscribe</Button>
         </Form>
-      </FooterSubscription>
+      </FooterSubscription> */}
       <FooterLinksContainer>
         <FooterLinksWrapper>
           <FooterLinkItems>
